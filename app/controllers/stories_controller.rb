e2b4efc82897631ea9113b4583ec0bc597d7b350
Bang Nguyen and James Turello <james@>
2016-09-08 10:07:37 -0700

    add story show route

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,7 +3,7 @@ class StoriesController < ApplicationController
     @stories = Story.all
   end
 
-  # def show
-  #   @story = Story.find(params[:id])
-  # end
+  def show
+    @story = Story.find(params[:id])
+  end
 end
