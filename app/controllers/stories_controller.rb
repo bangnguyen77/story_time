fe8f22eba1ba89f8cbc909da1727cbcab7cb05a0
Bang Nguyen and James Turello <bang@>
2016-09-08 10:30:45 -0700

    create new&create routes and views

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -6,4 +6,25 @@ class StoriesController < ApplicationController
   def show
     @story = Story.find(params[:id])
   end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      flash[:notice] = "Story saved successfully!"
+      redirect_to stories_path
+    else
+      flash[:alert] = "Story NOT saved. Try again!"
+      render :new
+    end
+  end
+
+  private
+  def story_params
+    params.require(:story).permit(:title, :image_url)
+  end
+
 end
